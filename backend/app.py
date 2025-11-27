28cb0e6347efcd940092978b816010ba3b60fb1b
shrutikadam08 <shrutikadam.nk@gmail.com>
2025-11-27 16:31:03 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -29,3 +29,15 @@ import uvicorn
 
 def start():
     uvicorn.run("backend.app:app",host="0.0.0.0", port=10000)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
