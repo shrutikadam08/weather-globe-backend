e053d921cfc9fda1169612b9a889e1cd43cc6bcb
shrutikadam08 <shrutikadam.nk@gmail.com>
2025-11-27 14:40:47 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-import fetcher
+from . import fetcher
 import os
 
 STATIC_DIR=os.path.join(os.path.dirname(__file__), "static")
